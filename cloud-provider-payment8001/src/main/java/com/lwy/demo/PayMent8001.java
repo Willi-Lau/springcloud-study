97d12e87a22661ce7e40c7afef991f91d3a83cc8
Willi_Lau <705195178@qq.com>
2021-04-01 20:13:50 +0800

    创建SpringCloud 父Project 子model

---
+++ cloud-provider-payment8001/src/main/java/com/lwy/demo/PayMent8001.java
@@ -0,0 +1,13 @@
+package com.lwy.demo;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayMent8001
+{
+    public static void main(String[] args) {
+        SpringApplication.run(PayMent8001.class,args);
+    }
+}
